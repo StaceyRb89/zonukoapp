196975a2bda6584ca235f7e4ecff989dc6a8ba36
StaceyRb89 <144110082+StaceyRb89@users.noreply.github.com>
2026-01-12 13:40:06 +0000

    Refine how it works copy

---
+++ zonuko/urls.py
@@ -0,0 +1,9 @@
+"""zonuko URL Configuration."""
+from django.contrib import admin
+from django.urls import include, path
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", include("apps.core.urls")),
+    path("users/", include("apps.users.urls")),
+]
