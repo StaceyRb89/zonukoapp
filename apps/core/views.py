9ab206712f7de1e4aa5f9579b90b4c15b7fee463
StaceyRb89 <144110082+StaceyRb89@users.noreply.github.com>
2026-01-13 11:29:44 +0000

    Fix age band migration order

---
+++ apps/core/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+
+def home(request):
+    return render(request, "core/home.html")
+
+
+def about(request):
+    return render(request, "core/about.html")
+
+
+def how_it_works(request):
+    return render(request, "core/how_it_works.html")
+
+
+def example_projects(request):
+    return render(request, "core/example_projects.html")
